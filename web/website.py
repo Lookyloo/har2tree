9c255f7aca587bcf1505b989be1e3ee4990fc5f5
Raphaël Vinot <raphael@vinot.info>
2017-06-25 22:13:39 +0200

    Add simple web interface

--- web/website.py
+++ web/website.py
@@ -1,21 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from har2tree import Har2Tree, setup_treestyle
 import json
 
-from ete3 import WebTreeApplication
+from har2tree import Har2Tree, hostname_treestyle
+from ete3_webserver import NodeActions, WebTreeHandler
+
+from flask import Flask, render_template  # , request
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.debug = True
 
 
-application = WebTreeApplication()
-application.CONFIG["temp_dir"] = './tmp/'
-application.CONFIG["temp_url"] = 'tmp'
-application.CONFIG["DISPLAY"] = ":0"
+def load_tree():
+    with open('../tests/data/libe.har', 'r') as f:
+        har = json.load(f)
+        har2tree = Har2Tree(har)
+    har2tree.make_tree()
+    actions = NodeActions()
+    style = hostname_treestyle()
+    return WebTreeHandler(har2tree.hostname_tree, actions, style)
 
 
-with open('../tests/data/libe.har', 'r') as f:
-    har = json.load(f)
-    har2tree = Har2Tree(har)
+@app.route('/', methods=['GET'])
+def index():
+    tree = load_tree()
+    nodes, faces, base64 = tree.redraw()
+    return render_template('tree.html', nodes=nodes, faces=faces, base64_img=base64)
 
-application.set_tree_loader(har2tree.make_tree)
-application.set_tree_style(setup_treestyle())
+if __name__ == '__main__':
+    load_tree()
+    app.run(port=5001)
