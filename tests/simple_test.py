2727a7977e1ba71e4cc4d26f76f03c0f2e069065
felalex57 <alexandre.felix@live.fr>
2021-04-22 17:03:33 +0200

    chg: Made tree name clearer for future trees additions

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -18,13 +18,14 @@ class SimpleTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         test_dir = Path(os.path.abspath(os.path.dirname(__file__))) / 'data' / 'simple'
         har_to_process = [test_dir / 'heroku_index.har']
-        cls.crawled_tree = CrawledTree(har_to_process, str(uuid.uuid4()))
+        cls.index_tree = CrawledTree(har_to_process, str(uuid.uuid4()))
+
 
     def test_root_url(self) -> None:
-        self.assertEqual(self.crawled_tree.root_url, 'https://lookyloo-testing.herokuapp.com/')
+        self.assertEqual(self.index_tree.root_url, 'https://lookyloo-testing.herokuapp.com/')
     
     def test_user_agent(self) -> None:
-        self.assertEqual(self.crawled_tree.user_agent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36")
+        self.assertEqual(self.index_tree.user_agent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36")
 
 
 if __name__ == '__main__':
