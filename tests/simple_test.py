9f43876efa2f8bd768f1469c21a7acfbfa993aa8
Raphaël Vinot <raphael@vinot.info>
2021-04-23 10:37:34 +0200

    fix: Make mypy happy

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -7,12 +7,10 @@ from pathlib import Path
 import os
 import uuid
 
-from typing import Iterable
-
 
 class SimpleTest(unittest.TestCase):
 
-    crawled_tree: CrawledTree
+    index_tree: CrawledTree
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -20,16 +18,12 @@ class SimpleTest(unittest.TestCase):
         har_to_process = [test_dir / 'heroku_index.har']
         cls.index_tree = CrawledTree(har_to_process, str(uuid.uuid4()))
 
-
     def test_root_url(self) -> None:
         self.assertEqual(self.index_tree.root_url, 'https://lookyloo-testing.herokuapp.com/')
-    
+
     def test_user_agent(self) -> None:
         self.assertEqual(self.index_tree.user_agent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36")
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
