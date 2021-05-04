e4968d033d344016b7b101d04572f01bfe9e0b26
Raphaël Vinot <raphael@vinot.info>
2021-05-04 13:26:24 +0200

    chg: make flake8 happy

--- har2tree/__init__.py
+++ har2tree/__init__.py
@@ -1,7 +1,7 @@
-from .parser import CrawledTree
-from .nodes import HostNode, URLNode, HarTreeNode
-from .har2tree import Har2Tree,  HarFile, Har2TreeLogAdapter
-from .helper import Har2TreeError
+from .parser import CrawledTree  # noqa
+from .nodes import HostNode, URLNode, HarTreeNode  # noqa
+from .har2tree import Har2Tree, HarFile, Har2TreeLogAdapter  # noqa
+from .helper import Har2TreeError  # noqa
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
