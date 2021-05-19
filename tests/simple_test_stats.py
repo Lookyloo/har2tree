fb2c65f4a8b42e05b20d39a91d9dfe64f32d50ee
felalex57 <alexandre.felix@live.fr>
2021-05-19 13:12:14 +0200

    fix: display more relevant information

--- tests/simple_test_stats.py
+++ tests/simple_test_stats.py
@@ -1,7 +1,12 @@
 import pstats
 from pstats import SortKey
-p = pstats.Stats('restats')
-p.strip_dirs()
-p.sort_stats(SortKey.CUMULATIVE)
-p.print_callers(100)
 
+# Read the file restats
+p = pstats.Stats('restats')
+
+# Remove unwanted directory paths that are way too long (remove if needed)
+p.strip_dirs()
+
+# Sort by cumulated time and display the first 25 first calls; then display their callers (the above functions)
+p.sort_stats(SortKey.CUMULATIVE).print_stats(25)
+p.print_callers(25)
