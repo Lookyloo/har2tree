05822e1c9a74cc9473b19d9bcf7d48c4a9fdb751
Raphaël Vinot <raphael@vinot.info>
2024-05-16 12:01:41 +0200

    new: Extract Google tag IDs from rendered page

--- har2tree/helper.py
+++ har2tree/helper.py
@@ -277,6 +277,14 @@ def find_identifiers(html_doc: bytes) -> dict[str, list[str]] | None:
                     else:
                         to_return[captcha].append(sitekey)
 
+    # This is beta and kinda fragile, but it's going to find (most) of the google tag IDs
+    # https://support.google.com/google-ads/answer/12326985?hl=en_us_us
+    # NOTE: the doc says 9 X, but all the examples I found have 10 X so we cannot trust it
+    if google_tag_ids := set(re.findall(rb"G-\w{9}+|AW-\w{9}+|GA-\w{9}+|UA-\w{9}+", html_doc)):
+        blocklist = {b'UA-Compatible'}
+        google_tag_ids -= blocklist
+        to_return['google_tag_ids'] = list(google_tag_ids)
+
     return to_return
 
 
